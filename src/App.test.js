7df467aac0a8202199098405296af24b902d3e75
yassinehaimouch <haymouch53@gmail.com>
2023-05-16 13:15:06 +0100

    Add Steps for TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('inputs should be initially empty', () => {
+  // 1) Rendering The Component We Want to Test
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // 2) Finding The Element
+  const emailInputElement = screen.getByRole("textbox");
+
+  // 3) Assertion
+  expect(emailInputElement.value).toBe("");
 });
